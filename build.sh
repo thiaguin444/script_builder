e7473ba2972a824d38b8c59b45426da52370fed4
thiaguin444 <thiagojurema@protonmail.com>
2025-05-07 11:17:11 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,38 @@
+# Update repos
+sudo apt update
+
+# Install dependencies
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
+# setup repo
+mkdir ~/bin
+
+# make path
+PATH=~/bin:$PATH
+
+# download repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+# make repo executable
+chmod a+x ~/bin/repo
+
+# setup git account
+git config --global user.name Thiago
+
+# setup email
+git config --global user.email thigo6617@gmail.com
+
+# make dir
+mkdir lineage2
+
+# cd
+cd lineage2
+
+# init repo
+repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs --depth=1
+
+# clone manifest
+git clone https://github.com/thiaguin444/local_manifests_odessa.git -b main .repo/local_manifests
+
+# sync
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j8
