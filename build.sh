4f57277b26ac1c77f43cd4046634707f7bc22c2b
thiaguin444 <thiagojurema@protonmail.com>
2025-05-08 10:49:02 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ sudo apt update
 # Install dependencies
 sudo apt install bc bison build-essential ccache libncurses5 kmod curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 
+# dependencies
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
+sudo apt install openjdk-8-jdk
+
 # setup repo
 mkdir ~/bin
 
@@ -29,7 +34,7 @@ mkdir lineage2
 cd lineage2
 
 # init repo
-repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 --git-lfs --depth=1
+repo init -u https://github.com/CherishOS/android_manifest.git -b twelve --depth=1
 
 # clone manifest
 git clone https://github.com/thiaguin444/local_manifests_odessa.git -b los21 .repo/local_manifests
