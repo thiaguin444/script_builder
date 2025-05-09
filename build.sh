2f48f19ffc6e8f7698d5dd200c26c47933abcbe4
thiaguin444 <thiagojurema@protonmail.com>
2025-05-08 21:35:36 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ mkdir lineage23
 cd lineage23
 
 # init repo
-repo init -u https://github.com/PixelExperience/manifest -b thirteen-plus --depth=1
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen --depth=1
 
 # clone manifest
 git clone https://github.com/thiaguin444/local_manifests_odessa.git -b los20 .repo/local_manifests
